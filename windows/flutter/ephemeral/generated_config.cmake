1df9ba4c5a075a409508cac5f2f021c3ad9c6367
flameXpriyanshu <103518761+flameXpriyanshu@users.noreply.github.com>
2023-12-14 00:02:22 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\kaith\\Downloads\\flutter_windows_3.3.2-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\projects\\melooha" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\kaith\\Downloads\\flutter_windows_3.3.2-stable\\flutter"
+  "PROJECT_DIR=D:\\projects\\melooha"
+  "FLUTTER_ROOT=C:\\Users\\kaith\\Downloads\\flutter_windows_3.3.2-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\projects\\melooha\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\projects\\melooha"
+  "FLUTTER_TARGET=D:\\projects\\melooha\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\projects\\melooha\\.dart_tool\\package_config.json"
+)
